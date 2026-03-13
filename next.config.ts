61d722bee2316a2ac611f9f5123896b07663c986
vanita02 <kambariyavanita2@gmail.com>
2026-03-13 18:35:21 +0530

    fix settings file errors

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   experimental: {
+//     serverComponentsExternalPackages: ['@prisma/client'],
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
-  },
+  serverExternalPackages: ["@prisma/client"],
+
   typescript: {
     ignoreBuildErrors: true,
   },
